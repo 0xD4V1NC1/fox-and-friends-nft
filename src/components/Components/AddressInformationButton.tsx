8a1a7305bec02e5b3088149d1e235ee3ac38687d
0xD4V1NC1 <0xD4V1NC1@gmail.com>
2022-08-28 14:15:10 -0400

    testnet set up

--- src/components/Components/AddressInformationButton.tsx
+++ src/components/Components/AddressInformationButton.tsx
@@ -17,7 +17,7 @@ const AddressInformationButton = ({address, connector}: {address:string, connect
       {/* <div id='address-information-gradient-border' className='p-0.5 bg-gradient-to-br from-primary-400 via-primary-500 to-secondary-500 rounded hover:bg-rainbow hover:animate-rainbow' >
         <Button type='button' ariaLabel='Click to disconnect wallet' color='primary-gradient-outline' text={`${formatWalletAddress(address, 10)}`} className='py-2 px-6' onClick={() => handleDisconnectWallet()}/>
       </div> */}
-      <Button type='button' ariaLabel='Click to disconnect wallet' color='primary' text={`${formatWalletAddress(address, 10)}`} className='py-2 px-6 hover:cursor-pointer' onClick={() => handleDisconnectWallet()} />
+      <Button type='button' ariaLabel='Click to disconnect wallet' color='primary-gradient' text={`${formatWalletAddress(address, 10)}`} className='py-2 px-6 hover:cursor-pointer' onClick={() => handleDisconnectWallet()} />
 
     </div>
   );
--- src/components/Components/ConnectWalletButton.tsx
+++ src/components/Components/ConnectWalletButton.tsx
@@ -10,7 +10,7 @@ const ConnectWalletButton = () => {
       {/* <div id='connect-wallet-gradient-border' className='p-0.5 bg-gradient-to-br from-primary-400 via-primary-500 to-secondary-500 rounded hover:bg-rainbow hover:animate-rainbow' >
         <Button type='button' ariaLabel='Click to connect wallet' color='primary-gradient-outline' text='Connect Wallet' className='py-2 px-6' onClick={() => setIsModalOpen(true)}/>
       </div> */}
-      <Button type='button' ariaLabel='Click to connect wallet' color='primary' text='Connect Wallet' className='py-2 px-6' onClick={() => setIsModalOpen(true)}/>
+      <Button type='button' ariaLabel='Click to connect wallet' color='primary-gradient' text='Connect Wallet' className='py-2 px-6' onClick={() => setIsModalOpen(true)}/>
 
       <ConnectWalletModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </>
